6bcf1021b87fc06c1e813eb8d99fcb68cbb56c94
Valentin Usatiy <Valekus777@gmail.com>
2018-08-04 16:35:13 +0300

    Selenide added

--- src/main/java/Pages/LoginToJiraPage.java
+++ src/main/java/Pages/LoginToJiraPage.java
@@ -2,20 +2,19 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class LoginToJiraPage {
-    public LoginToJiraPage(WebDriver driver){this.driver = driver;}
-    WebDriver driver;
-    String userNameXpath = "//*[@id=\"login-form-username\"]";
-    String userPassXpath = "//*[@id=\"login-form-password\"]";
-    String loginButtonXpath = "//*[@id=\"login-form-submit\"]";
+    String userNameXpath = "//*[@id=\'login-form-username\']";
+    String userPassXpath = "//*[@id=\'login-form-password\']";
+    String loginButtonXpath = "//*[@id=\"login\"]";                     //*[@id=\'login-form-submit\']";   //
     String onPage = "//body/div[1]/section/dashboard/div[1]/div/div[1]/div/div[1]/h1";
 
-    public void enterUserName (String userName) {driver.findElement(By.xpath(userNameXpath)).sendKeys(userName);}
-    public void enterPassword (String password){driver.findElement(By.xpath(userPassXpath)).sendKeys(password);}
-    public void clickSendButton(){driver.findElement(By.xpath(loginButtonXpath)).sendKeys(Keys.ENTER);}
+    public void enterUserName (String userName) {$(By.xpath(userNameXpath)).sendKeys(userName);}
+    public void enterPassword (String password){$(By.xpath(userPassXpath)).sendKeys(password);}
+    public void clickSendButton(){$(By.xpath(loginButtonXpath)).sendKeys(Keys.ENTER);}
     public boolean onPage(){
-        return driver.findElement(By.xpath(onPage)).isDisplayed();
+        return $(By.xpath(onPage)).isDisplayed();
     }
 }
