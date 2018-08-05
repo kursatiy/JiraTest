490496e363e10a387dee2705d8fa9ba1f37b4346
kursatiy <kursatiy@gmail.com>
2018-08-05 21:21:55 +0300

    added user preferences selectors

---
+++ src/main/java/Pages/UpdateUserPreferencesPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class UpdateUserPreferencesPage {
+    String userNameXpath = "//*[@id = 'update-user-preferences-pagesize']";
+    String emailTypeXpath = "//*[@id = 'update-user-preferences-mailtype']";
+    String languageXpath = "//*[@id = 'update-user-preferences-locale']";
+    String timeZoneRegionXpath = "//*[@id = 'timeZoneRegion']";
+    String timeZoneDefaultXpath = "//*[@id = 'defaultUserTimeZone']";
+    String sharingXpath = "//*[@id = 'shareDefault']";
+    String keyboardShortcutsXpath = "//*[@id = 'update-user-preferences-keyboard-shortcuts']";
+    String autowatchXpath = "//*[@id = 'update-user-preferences-auto-watch-preference']";
+    String updateButtonXpath = "//*[@id = 'update-user-preferences-submit']";
+    String cancelButtonXpath = "//*[@id = 'update-user-preferences-cancel']";
+
+}
